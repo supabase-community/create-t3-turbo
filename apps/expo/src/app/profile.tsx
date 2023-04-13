a635142f2d7c265ae9c2cb3bee86906eb534800d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-13 10:02:56 +0200

    add an email form

--- apps/expo/src/app/profile.tsx
+++ apps/expo/src/app/profile.tsx
@@ -1,4 +1,12 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Pressable,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import * as AppleAuthentication from "expo-apple-authentication";
 import { AntDesign } from "@expo/vector-icons";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
@@ -34,16 +42,6 @@ function SignedInView() {
 function SignedOutView() {
   const supabase = useSupabaseClient();
 
-  const signInWithGithub = async () => {
-    const { error, data } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-    console.log({ data });
-    if (error) return console.error(error.message);
-
-    // Open `data.url` on browser
-  };
-
   const signInWithApple = async () => {
     try {
       const { token, nonce } = await initiateAppleSignIn();
@@ -52,7 +50,7 @@ function SignedOutView() {
         token,
         nonce,
       });
-      if (error) throw error;
+      if (error) return Alert.alert("Error", error.message);
     } catch (e) {
       console.log("Error", e);
       if (typeof e === "object" && !!e && "code" in e) {
@@ -69,17 +67,16 @@ function SignedOutView() {
 
   return (
     <View className="space-y-4">
-      <Text className="text-2xl font-bold text-zinc-200">
-        You&apos;re signed out.
-      </Text>
-      <TouchableOpacity
-        onPress={signInWithGithub}
-        className="h-14 flex-row items-center justify-center gap-2 rounded-lg bg-zinc-200 p-2"
-      >
-        <AntDesign name="github" size={18} color="black" />
-        <Text className="text-xl font-medium">Sign In With Github</Text>
-      </TouchableOpacity>
+      <Text className="mb-4 text-2xl font-bold text-zinc-200">Sign In</Text>
 
+      {/* Email Sign In */}
+      <EmailForm />
+
+      <View className="relative mb-2 flex-row items-center justify-center border-b border-zinc-200 py-2">
+        <Text className="absolute top-1/2 bg-zinc-800 px-2 text-lg text-zinc-200">
+          or
+        </Text>
+      </View>
       {/* Sign in with Apple */}
       <AppleAuthentication.AppleAuthenticationButton
         buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
@@ -88,6 +85,76 @@ function SignedOutView() {
         cornerRadius={8}
         onPress={signInWithApple}
       />
+    </View>
+  );
+}
+
+function EmailForm() {
+  const supabase = useSupabaseClient();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const signInWithPassword = async () => {
+    const { error, data } = isSignUp
+      ? await supabase.auth.signUp({
+          email,
+          password,
+        })
+      : await supabase.auth.signInWithPassword({
+          email: "email",
+          password: "password",
+        });
+    if (error) return Alert.alert("Error", error.message);
+
+    console.log({ data });
+  };
+
+  return (
+    <View className="flex-col gap-4">
+      <TextInput
+        className="rounded bg-white/10 p-2 text-zinc-200"
+        placeholderTextColor="#A1A1A9" // zinc-400
+        value={email}
+        autoCapitalize="none"
+        onChangeText={setEmail}
+        placeholder="Email"
+      />
+      <View className="relative space-y-1">
+        <TextInput
+          className="rounded bg-white/10 p-2 text-zinc-200"
+          placeholderTextColor="#A1A1A9" // zinc-400
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+          autoCapitalize="none"
+          placeholder="Password"
+        />
+        <Pressable
+          className="absolute right-2"
+          onPress={() => setShowPassword((prev) => !prev)}
+        >
+          {showPassword && <AntDesign name="eye" size={24} color="#A1A1A9" />}
+          {!showPassword && <AntDesign name="eyeo" size={24} color="#A1A1A9" />}
+        </Pressable>
+      </View>
+
+      <Pressable className="h-4" onPress={() => setIsSignUp((prev) => !prev)}>
+        <Text className="flex-1 text-zinc-200">
+          {isSignUp ? "Already have an account?" : "Don't have an account?"}
+        </Text>
+      </Pressable>
+
+      <TouchableOpacity
+        onPress={signInWithPassword}
+        className="flex-row items-center justify-center rounded-lg bg-emerald-400 p-2"
+      >
+        <Text className="ml-1 text-xl font-medium">
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
