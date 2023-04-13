46b29d38d27b89db7ad262e7a7c0ae51143b84a5
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-04-13 18:01:37 +0900

    done with basic google login implementation

--- apps/expo/src/app/profile.tsx
+++ apps/expo/src/app/profile.tsx
@@ -1,9 +1,14 @@
+import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import * as AppleAuthentication from "expo-apple-authentication";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
 import { AntDesign } from "@expo/vector-icons";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 
 import { initiateAppleSignIn } from "../utils/auth";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Profile() {
   const user = useUser();
@@ -34,16 +39,6 @@ function SignedInView() {
 function SignedOutView() {
   const supabase = useSupabaseClient();
 
-  const signInWithGithub = async () => {
-    const { error, data } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-    console.log({ data });
-    if (error) return console.error(error.message);
-
-    // Open `data.url` on browser
-  };
-
   const signInWithApple = async () => {
     try {
       const { token, nonce } = await initiateAppleSignIn();
@@ -67,17 +62,52 @@ function SignedOutView() {
     }
   };
 
+  // Start Google login flow
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+    iosClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setToken(response.authentication!.accessToken);
+      getUserInfo();
+    }
+  }, [response, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+
+      const user = await response.json();
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+  // End google login flow
+
   return (
     <View className="space-y-4">
       <Text className="text-2xl font-bold text-zinc-200">
         You&apos;re signed out.
       </Text>
       <TouchableOpacity
-        onPress={signInWithGithub}
-        className="h-14 flex-row items-center justify-center gap-2 rounded-lg bg-zinc-200 p-2"
+        onPress={() => {
+          promptAsync();
+        }}
+        className="h-14 flex-row items-center justify-center gap-2 rounded-lg bg-white p-2"
       >
-        <AntDesign name="github" size={18} color="black" />
-        <Text className="text-xl font-medium">Sign In With Github</Text>
+        <AntDesign name="google" size={18} color="black" />
+        <Text className="text-xl font-medium">Sign In With Google</Text>
       </TouchableOpacity>
 
       {/* Sign in with Apple */}
